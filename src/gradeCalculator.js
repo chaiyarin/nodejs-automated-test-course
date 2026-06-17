fa1359a9f82900427dbe255c0e8865787d639953
Chaiyarin <chaiyarin.aof@gmail.com>
2026-06-17 18:15:26 +0700

    [LESSON] WS-1b gradeCalculator

---
+++ src/gradeCalculator.js
@@ -0,0 +1,17 @@
+// src/gradeCalculator.js
+
+function calculateGrade(score) {
+    if (score < 0 || score > 100) return 'Invalid Score';
+    if (score >= 90) return 'A';
+    if (score >= 85) return 'A-';
+    if (score >= 80) return 'B+';
+    if (score >= 75) return 'B';
+    if (score >= 70) return 'B-';
+    if (score >= 65) return 'C+';
+    if (score >= 60) return 'C';
+    if (score >= 55) return 'C-';
+    if (score >= 50) return 'D';
+    return 'F';
+}
+
+module.exports = { calculateGrade };
